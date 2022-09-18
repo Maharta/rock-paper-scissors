b031cc2aeb8c3005720a2995cda57a4c494490da
Maharta <gedemaharta.dev@gmail.com>
2022-09-18 14:02:27 +0800

    changed logic to declare winner, now using object/map

--- js/script.js
+++ js/script.js
@@ -5,36 +5,20 @@ function computerPlay() {
 }
 
 function play(playerInput, computerInput) {
-  switch (true) {
-    case playerInput === computerInput:
-      return "Draw!";
-      break;
-    case playerInput === "rock" && computerInput === "paper":
-      return "You lose, the computer choose paper!";
-      break;
+  const win = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper",
+  };
 
-    case playerInput === "rock" && computerInput === "scissors":
-      return "You win!, The computer choose scissors";
-      break;
+  const returnMessage = `you choose ${playerInput} and the computer choose ${computerInput}`;
 
-    case playerInput === "scissors" && computerInput === "rock":
-      return "You lose, the computer choose rock!";
-      break;
-
-    case playerInput === "scissors" && computerInput === "paper":
-      return "You win!, the computer choose paper";
-      break;
-
-    case playerInput === "paper" && computerInput === "scissors":
-      return "You lose, the computer choose scissors";
-      break;
-
-    case playerInput === "paper" && computerInput === "rock":
-      return "You win!, the computer choose rock";
-      break;
-
-    default:
-      return "Something went horribly wrong";
+  if (win[playerInput] === computerInput) {
+    return `${returnMessage}, you win!`;
+  } else if (win[computerInput] === playerInput) {
+    return `${returnMessage}, you lose!`;
+  } else {
+    return `${returnMessage}, draw!`;
   }
 }
 
